6acf7bbb1766347176614e5da395953786683e37
matthewb <mbacheld@charlotte.edu>
2025-02-12 22:15:52 -0500

    feat: updated bank account class to support subclasses

--- BankAccount.py
+++ BankAccount.py
@@ -1,37 +1,38 @@
-#Worked with Devdan
 class BankAccount:
     bank_title = "Bank of America"
-    def __init__(self,customer_name,current_balance,minimum_balance):
+    def __init__(self, account_number, customer_name, current_balance):
+        self._account_number = account_number
         self.customer_name = customer_name
         self.current_balance = current_balance
-        self.minimum_balance = minimum_balance
 
     def deposit(self,amount):
-        if (amount >= 0):
+        if amount >= 0:
             self.current_balance += amount
             print(f"Deposit Successful\n"
                   f"New Balance: {self.current_balance}\n")
         return
 
     def withdraw(self,amount):
-        if ((self.current_balance - amount) < self.minimum_balance):
-            print("Error: Amount would exceed minimum balance")
+        if self.current_balance <= 0 && self.current_balance > amount:
+            print("Withdraw Failed\n")
             return
         self.current_balance -= amount
 
     def print_customer_information(self):
         print(f"Bank Title: {self.bank_title}\n"
+               f"Account Number: {self._account_number}\n"
                f"Name: {self.customer_name}\n"
                f"Current Balance: {self.current_balance}\n")
 
-#Test instances:
-cust1 = BankAccount("Matthew B",20000,10000)
-cust1.deposit(100)
-cust1.print_customer_information()
-cust1.withdraw(100000)
-cust1.print_customer_information()
+def testing():
+    #Test instances:
+    cust1 = BankAccount(1,"Matthew B",20000)
+    cust1.deposit(100)
+    cust1.print_customer_information()
+    cust1.withdraw(100000)
+    cust1.print_customer_information()
 
-cust2 = BankAccount("Matt C",50000,6000)
-cust2.deposit(1000000)
-cust2.print_customer_information()
-cust2.withdraw(50000)
+    cust2 = BankAccount(2,"Matt C",50000)
+    cust2.deposit(1000000)
+    cust2.print_customer_information()
+    cust2.withdraw(50000)
